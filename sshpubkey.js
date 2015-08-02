834c66ee567997c5bd2054f9d3d4de929394df99
Melvin Carvalho <melvincarvalho@gmail.com>
2015-08-02 21:29:42 +0200

    sshpubkey cleanup

--- sshpubkey.js
+++ sshpubkey.js
@@ -1,79 +1,94 @@
 #!/usr/bin/env node
 
-// requires
+var fs = require("fs");
 var forge = require('node-forge');
-var fs    = require("fs");
-
-// init
-var sshpubkey = {};
-var pubkey;
+var debug = require('debug')('converter');
+var BigInteger = forge.jsbn.BigInteger;
 
 
-// process command line arguments : pubkeyFile
-var argv       = process.argv;
-var pubkeyfile = argv[2];
+/*
+* getField from asn.1
+*
+* @param {String} keyBytes
+* @param {int} start
+* @param {int} bytesLength
+**/
+function getField (keyBytes, start, bytesLength) {
+  bytesLength = bytesLength || 4;
+  // First get the length of the next field
+  var lengthEnd = start + bytesLength;
+  var lengthBytes = keyBytes.slice(start, lengthEnd);
+  var lengthValue = 0x100 * lengthBytes.charCodeAt(bytesLength - 2) + lengthBytes.charCodeAt(bytesLength - 1);
 
-
-// get pubkeyfile
-var home = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
-if (!pubkeyfile) {
-  pubkeyfile = home + '/.ssh/id_rsa.pub';
+  // Then get the field
+  var bytesEnd = lengthEnd + lengthValue;
+  var bytes = keyBytes.slice(lengthEnd, bytesEnd);
+  // return where the bytes start and the bytes
+  return {end: bytesEnd, bytes: bytes};
 }
-console.log('getting public key from : ' + pubkeyfile);
-try {
-  pubkey = fs.readFileSync(pubkeyfile, "utf8");
-} catch(e) {
-  console.log('could not load : ' + pubkeyfile);
-  process.exit(-1);
+
+
+
+function convertKeys (public, callback) {
+  fs.readFile(public, 'utf8', function (err, publicFile) {
+    if (err) return callback(err);
+
+    var convertedPublic = convertPublic(publicFile);
+    debug('Public key converted : ' + convertedPublic);
+
+    return callback(null, {
+      public: convertedPublic,
+    });
+
+  });
+}
+
+function convertPublic (publicFile) {
+  // Public Key
+
+  var bytesLength = 4;
+
+  // Get the main Key
+  var mainKey = publicFile.toString().split(' ')[1]; // [0] is prefix
+  var keyBytes = forge.util.decode64(mainKey);
+
+  // Get fields
+  var type = getField(keyBytes, 0);
+  var exponent = getField(keyBytes, type.end);
+  var modulus = getField(keyBytes, exponent.end);
+
+  // Convert pub key to big int
+  var convertedPublic = forge.pki.setRsaPublicKey(new BigInteger(forge.util.bytesToHex(modulus.bytes), 16), new BigInteger(forge.util.bytesToHex(exponent.bytes), 16));
+
+  return convertedPublic;
 }
 
 
-// get prefix
-sshpubkey.prefix = pubkey.toString().split(' ')[0];
-sshpubkey.mainkey = pubkey.toString().split(' ')[1];
+/*
+* Convert as a command
+*
+* @param {String} argv[2] login
+* @param {String} argv[3] public
+* @param {String} argv[4] private
+* @param {String} argv[5] pemFile
+* @callback {bin~cb} callback
+**/
+function bin(argv) {
+  var login = argv[2];
+  var public = argv[3] || './id_rsa.pub';
 
-sshpubkey.keybytes = forge.util.decode64(sshpubkey.mainkey);
-sshpubkey.keyhex = forge.util.bytesToHex(sshpubkey.keybytes);
-
-sshpubkey.byteslen = 4;
-
-// type
-//
-//
-sshpubkey.typelenstart = 0;
-sshpubkey.typelenend = sshpubkey.typelenstart + sshpubkey.byteslen;
-sshpubkey.typelenbytes = sshpubkey.keybytes.slice(sshpubkey.typelenstart, sshpubkey.typelenend);
-sshpubkey.typelen = sshpubkey.typelenbytes.charCodeAt(sshpubkey.byteslen-1);
-
-sshpubkey.typestart = sshpubkey.typelenend;
-sshpubkey.typeend = sshpubkey.typestart + sshpubkey.typelen;
-sshpubkey.typebytes = sshpubkey.keybytes.slice(sshpubkey.typestart, sshpubkey.typeend);
-
-// exponent
-//
-//
-sshpubkey.exponentlenstart = sshpubkey.typeend;
-sshpubkey.exponentlenend = sshpubkey.exponentlenstart + sshpubkey.byteslen;
-sshpubkey.exponentlenbytes = sshpubkey.keybytes.slice(sshpubkey.exponentlenstart, sshpubkey.exponentlenend);
-sshpubkey.exponentlen = sshpubkey.exponentlenbytes.charCodeAt(sshpubkey.byteslen-1);
-
-sshpubkey.exponentstart = sshpubkey.exponentlenend;
-sshpubkey.exponentend = sshpubkey.exponentstart + sshpubkey.exponentlen;
-sshpubkey.exponentbytes = sshpubkey.keybytes.slice(sshpubkey.exponentstart, sshpubkey.exponentend);
-
-// modulus
-//
-//
-sshpubkey.moduluslenstart = sshpubkey.exponentend;
-sshpubkey.moduluslenend = sshpubkey.moduluslenstart + sshpubkey.byteslen;
-sshpubkey.moduluslenbytes = sshpubkey.keybytes.slice(sshpubkey.moduluslenstart, sshpubkey.moduluslenend);
-sshpubkey.moduluslen = 256*sshpubkey.moduluslenbytes.charCodeAt(sshpubkey.byteslen-2) + sshpubkey.moduluslenbytes.charCodeAt(sshpubkey.byteslen-1);
-
-sshpubkey.modulusstart = sshpubkey.moduluslenend;
-sshpubkey.modulusend = sshpubkey.modulusstart + sshpubkey.moduluslen;
-sshpubkey.modulusbytes = sshpubkey.keybytes.slice(sshpubkey.modulusstart, sshpubkey.modulusend);
+  convertKeys(public, function(err, pub) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(pub);
+  });
+}
 
 
+// If one import this file, this is a module, otherwise a library
+if (require.main === module) {
+  bin(process.argv);
+}
 
-console.log(parseInt(forge.util.bytesToHex(sshpubkey.exponentbytes), 16));
-console.log(forge.util.bytesToHex(sshpubkey.modulusbytes));
+module.exports = convertKeys;
